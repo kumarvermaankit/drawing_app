dedf31ce1c0f5d45d3e4db796c5c80b33f46fd83
kumar <kumarvermaankit9555@gmail.com>
2021-05-15 10:29:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useRef, useState} from "react"
 
 function App() {
+
+const Canvasref=useRef(null);
+const Contextref=useRef(null)
+
+const [isDrawing,setisDrawing]=useState(false)
+
+
+useEffect(()=>{
+
+const canvas=Canvasref.current;
+
+canvas.width=window.innerWidth*2;
+canvas.height=window.innerHeight*2;
+canvas.style.width=`${window.innerWidth}px`
+canvas.style.height=`${window.innerHeight}px`
+
+const context=canvas.getContext("2d")
+context.scale(2,2)
+
+context.lineCap="round"
+context.strokeStyle="black"
+context.lineWidth=5
+Contextref.current=context
+
+
+},[])
+
+function Startdrawing(nativeEvent){
+ 
+const {offsetX,offsetY}=nativeEvent.nativeEvent;
+Contextref.current.beginPath();
+Contextref.current.moveTo(offsetX,offsetY)
+setisDrawing(true)
+}
+
+function finishdrawing(nativeEvent){
+ 
+Contextref.current.closePath()
+setisDrawing(false)
+
+}
+
+function draw(nativeEvent){
+if(!isDrawing){
+  return;
+}
+  const {offsetX,offsetY}=nativeEvent.nativeEvent;
+console.log(offsetX)
+  Contextref.current.lineTo(offsetX,offsetY);
+  Contextref.current.stroke()
+
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+ <canvas 
+onMouseDown={Startdrawing}
+onMouseUp={finishdrawing}
+onMouseMove={draw}
+ref={Canvasref}
+
+
+ />
+   
   );
 }
 
